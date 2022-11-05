f8146d353e065fe99431c6f04870e0fa71743a7e
Bikash Dash <radharbikashdash@gmail.com>
2022-11-06 00:32:36 +0530

    generic

---
+++ book/plug.ts
@@ -0,0 +1,22 @@
+import {  } from "./generic";
+
+class plug {
+    constructor(pn) {
+        let drive = "SDCZ50-032G-I35";
+        let title = "sandisk";
+        let name=generic('ufd")
+    }
+}
+
+
+// .plug{
+//     .SDCZ50-032G-I35{
+//         tittle : "sandisk";
+//         name:generic('ufd");
+//         description :generic('usb")
+//         origin : country('+60")
+//         cz50 :32GB
+//         1GB : 1,000,000,000 bytes
+//         info : storage also used for memory optimization and performance. user storage is less then full capacity
+//     }
+// }
