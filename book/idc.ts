92d695b0957b1093ec9dc1cc2dc0444c9fd9220f
Bikash Dash <radharbikashdash@gmail.com>
2022-11-05 23:45:08 +0530

    updated idc@tab && port@folder && removed readme@generic-port-idc

---
+++ book/idc.ts
@@ -0,0 +1,8 @@
+class idc {
+    constructor(tab) {
+        
+        let id=111390401;
+        let reference=82211078908;
+        let meter = 56516769;
+    }
+}
---
+++ book/port.ts
@@ -0,0 +1,7 @@
+class port {
+    constructor(folder) {
+        let home = "5205:13853";
+        let bill = "5205:13854";
+    }
+}
+
